01a5df94e9cefd8b71e3880e500fdc8263a58886
Arron Dick <s_drac2@yahoo.com>
2016-02-20 13:24:53 +1300

    Bridge Dealing Done

--- BridgeDealingProgram/BridgeDealingProgram/Enums.cs
+++ BridgeDealingProgram/BridgeDealingProgram/Enums.cs
@@ -9,29 +9,37 @@ namespace BridgeDealingProgram
     public enum Enums
     {
     }
-        public enum Suit
-        {
-            CLUBS = 1,
-            DIAMONDS,
-            HEARTS,
-            SPADES,
-        }
-
-        public enum Rank
-        {
-            ACE = 1,
-            TWO,
-            THREE,
-            FOUR,
-            FIVE,
-            SIX,
-            SEVEN,
-            EIGHT,
-            NINE,
-            TEN,
-            JACK,
-            QUEEN,
-            KING,
-        }
+    public enum Suit
+    {
+        SPADE = 0,
+        HEART,
+        DIAMOND,
+        CLUB
     }
+
+    public enum Rank
+    {
+        TWO = 2,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE
+    }
+
+    public enum Seat
+    {
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST
+    }
+}
 
