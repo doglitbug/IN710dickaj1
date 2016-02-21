a29af9770d9734ff9dfb969dd32c61808c435576
Arron Dick <s_drac2@yahoo.com>
2016-02-21 16:01:48 +1300

    Bridge Dealing App in form

---
+++ BridgeDealingApplication/BridgeDealingApplication/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BridgeDealingApplication
+{
+    public partial class Form1 : Form
+    {
+        private const int NPLAYERS = 4;
+        private const int NSIZEOFHAND = 13;
+        //Card deck
+        Deck myDeck;
+        //Hold the players hands
+        Hand[] theHands;
+
+        public Form1()
+        {
+            //Random number generator
+            Random rand = new Random();
+
+            //Create a new playing deck
+            myDeck = new Deck(rand);
+
+            InitializeComponent();
+        }
+
+        private void buttonDeal_Click(object sender, EventArgs e)
+        {
+            dealAGame();
+            displayHands();
+        }
+
+        private void dealAGame()
+        {
+            //Shuffle the cards in the deck
+            myDeck.Shuffle();
+
+            //Reset the players hands
+            theHands = new Hand[NPLAYERS];
+
+            //Deal the hands and return to a hand array
+            theHands = myDeck.DealHands(NPLAYERS, NSIZEOFHAND);
+        }
+
+        private void displayHands()
+        {
+            //Clear previous game from listBox
+            listBoxOutput.Items.Clear();
+
+            //Loop through all hands
+            for (int i = 0; i < theHands.Length; i++)
+            {
+                //Display hand in listBox
+
+                //Get hand name and display
+                String name=((Seat)i).ToString();
+                listBoxOutput.Items.Add(name);
+                listBoxOutput.Items.Add("--------");
+
+                //Display cards in the hand
+
+                //Get the cards
+                Card[] cards = theHands[i].getCards();
+                //Loop through them
+                for (int j = 0; j < cards.Length; j++)
+                {
+                    //Add card to listbox
+                    listBoxOutput.Items.Add(cards[j]);
+                }
+
+                    //Seperator at bottom of display
+                    listBoxOutput.Items.Add("");
+                    listBoxOutput.Items.Add("");
+            }
+
+        }
+    }
+}
---
+++ BridgeDealingApplication/BridgeDealingApplication/Hand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeDealingApplication
+{
+    class Hand
+    {
+        private Card[] cards;
+        /// <summary>
+        /// Create a new Hand
+        /// </summary>
+        /// <param name="cards">An array of cards to place in the hand</param>
+        public Hand(Card[] cards)
+        {
+            this.cards = cards;
+        }
+
+        /// <summary>
+        /// Goes through all cards in the hand and counts up the High Card Points
+        /// </summary>
+        /// <returns>Total High Card Points</returns>
+        public int getHCP()
+        {
+            int value = 0;
+            for (int i = 0; i < cards.Length; i++)
+            {
+                value += cards[i].getHCP();
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Accessor for cards in the hand
+        /// </summary>
+        /// <returns>Array of cards</returns>
+        public Card[] getCards(){
+            return this.cards;
+        }
+    }
+}
