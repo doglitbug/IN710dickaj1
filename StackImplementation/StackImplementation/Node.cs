4b16f9271cd5db6198b9559c95b586a4b4f0ea9d
Arron Dick <s_drac2@yahoo.com>
2016-03-05 15:29:41 +1300

    Added node and code

---
+++ StackImplementation/StackImplementation/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplementation
+{
+    class Node
+    {
+        public String Data { get; set;}
+        public Node Next { get; set;}
+        public Node(String data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
