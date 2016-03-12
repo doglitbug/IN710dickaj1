59006119d0723aea09b0cd31e65a95484e4d6b47
Arron Dick <s_drac2@yahoo.com>
2016-03-12 22:59:32 +1300

    Finished Clocks

--- Strategy Patterns/Clocks/Clocks/DigitalClock.cs
+++ Strategy Patterns/Clocks/Clocks/DigitalClock.cs
@@ -2,35 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Timers;
 using System.Windows.Forms;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Clocks
 {
     class DigitalClock:IClock
 
     {
-        Label clockLabel;
-       
+        private Label clockLabel;
+        private System.Timers.Timer aTimer;
+        // This delegate enables asynchronous calls for setting
+        // the text property on a TextBox control.
+        delegate void SetTextCallback(string text);
 
         public DigitalClock(Label clockLabel)
         {
+            //Set label used to display time
             this.clockLabel = clockLabel;
+            //Create new timer to update this clock
+            aTimer = new System.Timers.Timer(2000);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
 
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            setText(DateTime.Now.ToString("HH:mm:ss tt"));
+        }
+
+        private void setText(String text)
+        {
+            if (this.clockLabel.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(setText);
+                clockLabel.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.clockLabel.Text = text;
+            }
         }
         public void On()
         {
-            throw new NotImplementedException();
+            aTimer.Enabled = true;
         }
 
         public void Off()
         {
-            throw new NotImplementedException();
+            aTimer.Enabled = false;
         }
 
         public void UpdateTimeDisplay()
         {
-            throw new NotImplementedException();
+            
         }
 
         public void ShowClock()
