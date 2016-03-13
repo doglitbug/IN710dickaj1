7830da45fed2fcaa271a81dbbf00cf0cb7c86e97
Arron Dick <s_drac2@yahoo.com>
2016-03-14 12:06:46 +1300

    Cleaned up comments. noticed unneed line 45 on Form1.cs?

--- Strategy Patterns/Animal Shelter/IN710 4.1 Animal Shelter Solution 2014/PictureDisplay.cs
+++ Strategy Patterns/Animal Shelter/IN710 4.1 Animal Shelter Solution 2014/PictureDisplay.cs
@@ -9,23 +9,35 @@ namespace IN710_4._1_Animal_Shelter_Solution_2014
 {
     class PictureDisplay : IDisplay
     {
+        /// <summary>
+        /// Reference to the picture boxes from the form
+        /// </summary>
         List<PictureBox> pictureBoxList;
-        /* YOUR CODE HERE */
         public PictureDisplay(List<PictureBox> pictureBoxList)
         {
             this.pictureBoxList = pictureBoxList;
         }
+        /// <summary>
+        /// Display pictures for the given critterList
+        /// </summary>
+        /// <param name="critterList">List of critters</param>
         public void displayCritterList(List<Critter> critterList)
         {
             //Loop over all the critters and display a picture for each
+            //Double condition in for loop so that less critters than pictureboxes can be displayed
             for (int i = 0; i < pictureBoxList.Count && i < critterList.Count; i++)
             {
+                //Set picture box image to that of critter
                 pictureBoxList[i].ImageLocation = critterList[i].ImageFileName;
             }
         }
 
+        /// <summary>
+        /// Clear all picture boxes
+        /// </summary>
         public void clearDisplay()
         {
+            //Loop over each picture box and set image to null
             foreach (PictureBox pb in pictureBoxList)
             {
                 pb.Image = null;
--- Strategy Patterns/Animal Shelter/IN710 4.1 Animal Shelter Solution 2014/TextDisplay.cs
+++ Strategy Patterns/Animal Shelter/IN710 4.1 Animal Shelter Solution 2014/TextDisplay.cs
@@ -6,19 +6,30 @@ using System.Windows.Forms;
 
 namespace IN710_4._1_Animal_Shelter_Solution_2014
 {
-    class TextDisplay: IDisplay
+    class TextDisplay : IDisplay
     {
-        /* YOUR CODE HERE */
+        /// <summary>
+        /// ListBox to display names into
+        /// </summary>
         ListBox theListBox;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="theListBox">Reference to listBox to display items in</param>
         public TextDisplay(ListBox theListBox)
         {
             this.theListBox = theListBox;
         }
+        /// <summary>
+        /// Display the list of critters given in the listbox
+        /// </summary>
+        /// <param name="critterList">List of critters</param>
         public void displayCritterList(List<Critter> critterList)
         {
+            //Loop over each critter in ther list and add to listbox
             foreach (Critter c in critterList)
             {
-                theListBox.Items.Add(c);
+                theListBox.Items.Add(c.Name);
             }
         }
 
