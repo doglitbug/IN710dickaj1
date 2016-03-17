f8371fbff7cc4e3f40ae548b2ad3ccc71b743819
Arron Dick <s_drac2@yahoo.com>
2016-03-18 09:02:32 +1300

    Added Eagle

---
+++ Factory Patterns/AnimalWorld/AnimalWorld/Animals/Eagle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalWorld
+{
+    public class Eagle:Animal
+    {
+        public Eagle()
+        {
+            name = "Eagle";
+            foodType = "Carnivore";
+            preferedFood = "Fish";
+            //TODO find image and update here
+            image = "";
+        }
+    }
+}
