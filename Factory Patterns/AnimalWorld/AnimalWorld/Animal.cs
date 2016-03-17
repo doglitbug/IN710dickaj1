13c5d248edeeac38823172150082da84d65bce61
Arron Dick <s_drac2@yahoo.com>
2016-03-18 08:55:34 +1300

    Animal class completed

---
+++ Factory Patterns/AnimalWorld/AnimalWorld/Animal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalWorld
+{
+    class Animal
+    {
+        /// <summary>
+        /// Type of animal, eg Bison or wolf
+        /// </summary>
+        protected string name;
+        /// <summary>
+        /// Herbivore or carnivore
+        /// TODO refactor into enum?
+        /// </summary>
+        protected string foodType;
+        /// <summary>
+        /// What they prefer to eat, eg fish or grass
+        /// </summary>
+        protected string preferedFood;
+        /// <summary>
+        /// Image for animal
+        /// </summary>
+        protected Bitmap image;
+
+
+        //Getter/setters
+
+        /// <summary>
+        /// get image of animal as a Bitmap
+        /// </summary>
+        public Bitmap Image
+        { get { return image; } }
+
+        //Methods
+
+        /// <summary>
+        /// String representation of this animal
+        /// </summary>
+        public override string toString(){
+            string displayString="I am "+name;
+            displayString+=". I am a "+foodType;
+            displayString+=". I eat "+preferedFood;
+            return displayString;
+        }
+    }
+}
