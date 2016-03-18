39846cd64bd54e578cfa121681ca5ec562a2289b
Arron Dick <s_drac2@yahoo.com>
2016-03-18 21:19:06 +1300

    Started Computer App and added modelling project

---
+++ Factory Patterns/AnimalWorld/AnimalWorld/IAnimalFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalWorld
+{
+    
+    /// <summary>
+    /// Interface for an animal factory
+    /// </summary>
+    public interface IAnimalFactory
+    {
+        /// <summary>
+        /// Creates a new animal given the animal code
+        /// </summary>
+        /// <param name="animalCode">Integer representing which animal to create</param>
+        /// <returns>New animal</returns>
+        Animal createAnimal(int animalCode);
+    }
+}
