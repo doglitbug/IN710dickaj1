33e40fba1d1ed0d8b0675ffab06563ae39e8dd57
Arron Dick <s_drac2@yahoo.com>
2016-03-21 21:13:52 +1300

    Added methods to grab parts based on name only as this will be passed from form

--- Factory Patterns/MixAndMatch/MixAndMatch/CharacterStorage.cs
+++ Factory Patterns/MixAndMatch/MixAndMatch/CharacterStorage.cs
@@ -11,7 +11,7 @@ namespace MixAndMatch
         List<Character> avaliableCharacters;
         public CharacterStorage()
         {
-            avaliableCharacters=new List<Character>();
+            avaliableCharacters = new List<Character>();
             //TODO add in all characters at this point!
             avaliableCharacters.Add(new Fairy());
             avaliableCharacters.Add(new Frankenstein());
@@ -24,12 +24,67 @@ namespace MixAndMatch
         /// <returns>String List of character names</returns>
         public List<string> getListofCharacters()
         {
-            List<string> names=new List<string>();
+            List<string> names = new List<string>();
             foreach (Character c in avaliableCharacters)
             {
                 names.Add(c.Name);
             }
             return names;
         }
+
+        /// <summary>
+        /// Get a head based off the characters name
+        /// </summary>
+        /// <param name="characterName">Name of the character</param>
+        /// <returns>The characters head</returns>
+        public Head getHeadFromSelection(string characterName)
+        {
+            Head selectedHead = null;
+            foreach (Character c in avaliableCharacters)
+            {
+                if (c.Name == characterName)
+                {
+                    selectedHead = c.myHead;
+                }
+            }
+            return selectedHead;
+        }
+
+        /// <summary>
+        /// Get a Body based off the characters name
+        /// </summary>
+        /// <param name="characterName">Name of the character</param>
+        /// <returns>The characters Body</returns>
+        public Body getBodyFromSelection(string characterName)
+        {
+            Body selectedBody = null;
+            foreach (Character c in avaliableCharacters)
+            {
+                if (c.Name == characterName)
+                {
+                    selectedBody = c.myBody;
+                }
+            }
+            return selectedBody;
+        }
+
+        /// <summary>
+        /// Get a Legs based off the characters name
+        /// </summary>
+        /// <param name="characterName">Name of the character</param>
+        /// <returns>The characters Legs</returns>
+        public Legs getLegsFromSelection(string characterName)
+        {
+            Legs selectedLegs = null;
+            foreach (Character c in avaliableCharacters)
+            {
+                if (c.Name == characterName)
+                {
+                    selectedLegs = c.myLegs;
+                }
+            }
+            return selectedLegs;
+        }
     }
 }
+
