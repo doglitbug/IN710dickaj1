d6a5d06fa13c531f72730f2d47ef884f46a801ad
Arron Dick <s_drac2@yahoo.com>
2016-03-22 14:12:28 +1300

    Added new project and interfaces

---
+++ Observer Patterns/BicycleDashboard/BicycleDashboard/ISubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BicycleDashboard
+{
+    interface ISubject
+    {
+        void AddObserver(IObserver o);
+        void Removeobserver(IObserver o);
+        void NotifyObservers();
+    }
+}
