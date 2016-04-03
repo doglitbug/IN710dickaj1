9d72c92517aa594b5771d1fb336bd6bac47f0d98
Arron Dick <s_drac2@yahoo.com>
2016-04-03 15:19:58 +1200

    Added code for progress method and reset controls

--- Delegates/ProgressIndicator/ProgressIndicator/Form1.cs
+++ Delegates/ProgressIndicator/ProgressIndicator/Form1.cs
@@ -12,7 +12,7 @@ namespace ProgressIndicator
 {
     public partial class Form1 : Form
     {
-        public delegate void trackProgress();
+        public delegate void progressDelegate();
 
         public Form1()
         {
@@ -21,6 +21,7 @@ namespace ProgressIndicator
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            progressDelegate selectedDelegate;
             if (rbSpinBox.Checked)
             {
                 //TODO Do spin box
@@ -32,6 +33,34 @@ namespace ProgressIndicator
             else
             {
                 //TODO Do track bar
+            }
+
+            resetControls();
+            slowMethod(selectedDelegate);
+        }
+
+
+        /// <summary>
+        /// Resets all three form controls to default values
+        /// </summary>
+        private void resetControls()
+        {
+            numericUpDown.Value = 0;
+            probOutput.Value=0;
+            trackBar1.Value = 0;
+        }
+        /// <summary>
+        /// Show progress from 0-100% in 10% steps
+        /// </summary>
+        /// <param name="myProgressDelegate">Delegate to run for each 10% of progress</param>
+        private void slowMethod(progressDelegate myProgressDelegate)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //Wait for a bit so progress is visible
+                System.Threading.Thread.Sleep(1000);
+                //Increment and display some progress
+                myProgressDelegate();
             }
         }
     }
