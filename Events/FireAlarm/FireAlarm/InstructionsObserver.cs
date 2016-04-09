dd39dbd60805c6e9522cda2568ba02e8e4a04db1
Arron Dick <s_drac2@yahoo.com>
2016-04-10 09:49:06 +1200

    Added instructions observer

---
+++ Events/FireAlarm/FireAlarm/InstructionsObserver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FireAlarm
+{
+    public class InstructionsObserver:FireAlarmObserverBase
+    {
+        /// <summary>
+        /// Constuctor, passes argument straight to parent which will register
+        /// </summary>
+        /// <param name="subject">Subject to observe</param>
+        public InstructionsObserver(FireAlarmSubject subject) : base(subject) { }
+
+        /// <summary>
+        /// Method that is executed when event is fired
+        /// </summary>
+        /// <param name="fireSubject">Subject that fired event</param>
+        /// <param name="fe">Event arguments</param>
+        public override void FireAlarmHandlerMethod(object fireSubject, FireAlarmEventArgs fe)
+        {
+            String message = "Fire is " + fe.FireCategory.ToString() + ". ";
+            switch (fe.FireCategory)
+            {
+                case EFireCategory.MINOR:
+                    message += "Use a fire extinguisher.";
+                    break;
+                case EFireCategory.SERIOUS:
+                    message += "Call the Fire Department.";
+                    break;
+                case EFireCategory.INFERNO:
+                    message += "Evacuate immediately!!!!!";
+                    break;
+            }
+            MessageBox.Show(message);
+        }
+    }
+}
