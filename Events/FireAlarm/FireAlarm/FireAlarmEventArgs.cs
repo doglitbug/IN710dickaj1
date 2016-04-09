2e291b0262e293bb8a31084e767c237319f476c4
Arron Dick <s_drac2@yahoo.com>
2016-04-10 09:15:37 +1200

    Added custom event args

---
+++ Events/FireAlarm/FireAlarm/FireAlarmEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireAlarm
+{
+    public enum EFireCategory { MINOR, SERIOUS, INFERNO};
+    public class FireAlarmEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Current category of fire
+        /// </summary>
+        public EFireCategory FireCategory { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="currentFireCatgeory">Enum for current category</param>
+        public FireAlarmEventArgs(EFireCategory currentFireCatgeory){
+            FireCategory=currentFireCatgeory;
+        }
+    }
+}
