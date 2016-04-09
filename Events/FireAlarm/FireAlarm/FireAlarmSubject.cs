b51eddbc8ee567b2311570dcc9f9e6e9ea92b4e1
Arron Dick <s_drac2@yahoo.com>
2016-04-10 09:27:25 +1200

    Added firealarm subject

---
+++ Events/FireAlarm/FireAlarm/FireAlarmSubject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireAlarm
+{
+    public class FireAlarmSubject
+    {
+        public delegate void FireEventHandler(object fireSubject, FireAlarmEventArgs fe);
+        public event FireEventHandler FireEvent;
+
+        public void onFireEvent(EFireCategory fCat)
+        {
+            //Create new custom event args using supplied category
+            FireAlarmEventArgs fe = new FireAlarmEventArgs(fCat);
+
+            //Check there is a observer registed or system will crash
+            if (FireEvent != null)
+            {
+                FireEvent(this, fe);
+            }
+        }
+    }
+}
