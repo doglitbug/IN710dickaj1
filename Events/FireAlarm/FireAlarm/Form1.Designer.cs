8dcde554a1c72dbfe2e825281f7158c963829bb0
Arron Dick <s_drac2@yahoo.com>
2016-04-10 09:11:23 +1200

    Designed form

--- Events/FireAlarm/FireAlarm/Form1.Designer.cs
+++ Events/FireAlarm/FireAlarm/Form1.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAlarm = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbMinor = new System.Windows.Forms.RadioButton();
+            this.rbSerious = new System.Windows.Forms.RadioButton();
+            this.rbInferno = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAlarm
+            // 
+            this.btnAlarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlarm.Location = new System.Drawing.Point(12, 12);
+            this.btnAlarm.Name = "btnAlarm";
+            this.btnAlarm.Size = new System.Drawing.Size(144, 43);
+            this.btnAlarm.TabIndex = 0;
+            this.btnAlarm.Text = "Fire Alarm";
+            this.btnAlarm.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbInferno);
+            this.groupBox1.Controls.Add(this.rbSerious);
+            this.groupBox1.Controls.Add(this.rbMinor);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 61);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(144, 137);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fire Category";
+            // 
+            // rbMinor
+            // 
+            this.rbMinor.AutoSize = true;
+            this.rbMinor.Location = new System.Drawing.Point(7, 28);
+            this.rbMinor.Name = "rbMinor";
+            this.rbMinor.Size = new System.Drawing.Size(76, 28);
+            this.rbMinor.TabIndex = 0;
+            this.rbMinor.TabStop = true;
+            this.rbMinor.Text = "Minor";
+            this.rbMinor.UseVisualStyleBackColor = true;
+            // 
+            // rbSerious
+            // 
+            this.rbSerious.AutoSize = true;
+            this.rbSerious.Location = new System.Drawing.Point(7, 62);
+            this.rbSerious.Name = "rbSerious";
+            this.rbSerious.Size = new System.Drawing.Size(92, 28);
+            this.rbSerious.TabIndex = 1;
+            this.rbSerious.TabStop = true;
+            this.rbSerious.Text = "Serious";
+            this.rbSerious.UseVisualStyleBackColor = true;
+            // 
+            // rbInferno
+            // 
+            this.rbInferno.AutoSize = true;
+            this.rbInferno.Location = new System.Drawing.Point(7, 96);
+            this.rbInferno.Name = "rbInferno";
+            this.rbInferno.Size = new System.Drawing.Size(86, 28);
+            this.rbInferno.TabIndex = 2;
+            this.rbInferno.TabStop = true;
+            this.rbInferno.Text = "Inferno";
+            this.rbInferno.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(173, 211);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnAlarm);
+            this.Name = "Form1";
+            this.Text = "Fire Alarm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAlarm;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbInferno;
+        private System.Windows.Forms.RadioButton rbSerious;
+        private System.Windows.Forms.RadioButton rbMinor;
     }
 }
 
