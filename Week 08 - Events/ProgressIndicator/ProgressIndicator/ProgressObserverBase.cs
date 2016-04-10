e1817bac3024b7c97efa578417905c798e9c65a8
Arron Dick <s_drac2@yahoo.com>
2016-04-10 12:56:11 +1200

    Added observer base class

---
+++ Week 08 - Events/ProgressIndicator/ProgressIndicator/ProgressObserverBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressIndicator
+{
+    public abstract class ProgressObserverBase
+    {
+        protected ProgressSubject subject;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="subject">Subject to observe</param>
+        public ProgressObserverBase(ProgressSubject subject)
+        {
+            //Keep reference to the subject
+            this.subject = subject;
+            //Create a new handler
+            EventHandler handler = new EventHandler(incrementDisplay);
+            //Register handler with subject;
+            subject.updateEvent+=handler;
+        }
+
+        public abstract void incrementDisplay(object subject, EventArgs ea);
+    }
+}
