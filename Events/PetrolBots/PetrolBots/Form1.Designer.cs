ebb750240fa1245e338df4afab5989cf8c9ce2a0
Arron Dick <s_drac2@yahoo.com>
2016-04-10 14:27:37 +1200

    Creation of new world in form load

--- Events/PetrolBots/PetrolBots/Form1.Designer.cs
+++ Events/PetrolBots/PetrolBots/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.ClientSize = new System.Drawing.Size(484, 461);
             this.Name = "Form1";
             this.Text = "Petrol Bots";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- Events/PetrolBots/PetrolBots/Form1.cs
+++ Events/PetrolBots/PetrolBots/Form1.cs
@@ -12,9 +12,26 @@ namespace PetrolBots
 {
     public partial class Form1 : Form
     {
+        private World world;
+        private Random random;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //create a new rng
+            random = new Random();
+
+            //Get worlds max size
+            Point world_size=new Point(this.Width,this.Height);
+            
+            //Get the graphics object
+            Graphics mainCanvas = this.CreateGraphics();
+
+            //Create a new World
+            world = new World(random, world_size,mainCanvas);
         }
     }
 }
