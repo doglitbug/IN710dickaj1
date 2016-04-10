2728a731cd0b52d7ebb174c1558be39d78853df2
Arron Dick <s_drac2@yahoo.com>
2016-04-10 12:29:29 +1200

    Stripped out old code from form

--- Week 08 - Events/ProgressIndicator/ProgressIndicator/Form1.cs
+++ Week 08 - Events/ProgressIndicator/ProgressIndicator/Form1.cs
@@ -12,8 +12,6 @@ namespace ProgressIndicator
 {
     public partial class Form1 : Form
     {
-        public delegate void progressDelegate();
-
         public Form1()
         {
             InitializeComponent();
@@ -21,48 +19,7 @@ namespace ProgressIndicator
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            progressDelegate selectedDelegate;
-            if (rbSpinBox.Checked)
-            {
-                selectedDelegate = new progressDelegate(spinBoxUpdater);
-            }
-            else if (rbProgressBar.Checked)
-            {
-                selectedDelegate = new progressDelegate(progressBarUpdater);
-            }
-            else
-            {
-                //trackbar
-                selectedDelegate = new progressDelegate(trackBarUpdater);
-            }
-
-            resetControls();
-            slowMethod(selectedDelegate);
-        }
-        /// <summary>
-        /// Updates spin box control
-        /// </summary>
-        private void spinBoxUpdater()
-        {
-            //TODO Check this is correct as it might not account for step sizes =/= 1
-            numericUpDown.Value++;
-        }
-
-        /// <summary>
-        /// Updates progress bar control
-        /// </summary>
-        private void progressBarUpdater()
-        {
-            probOutput.PerformStep();
-        }
-
-        /// <summary>
-        /// updates a trackbar control
-        /// </summary>
-        private void trackBarUpdater()
-        {
-            //TODO Check this is correct as it might not account for step sizes =/= 1
-            trackBar1.Value++;
+            //TODO slowWorker.SlowMethod();
         }
 
         /// <summary>
@@ -74,22 +31,6 @@ namespace ProgressIndicator
             probOutput.Value=0;
             trackBar1.Value = 0;
         }
-        /// <summary>
-        /// Show progress from 0-100% in 10% steps
-        /// </summary>
-        /// <param name="myProgressDelegate">Delegate to run for each 10% of progress</param>
-        private void slowMethod(progressDelegate myProgressDelegate)
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                //Wait for a bit so progress is visible
-                //TODO Replace this as it makes the UI unresponsive
-                System.Threading.Thread.Sleep(500);
-                //Increment the given progress diplayer
-                myProgressDelegate();
-                //Force form to update
-                Application.DoEvents();
-            }
-        }
+
     }
 }
