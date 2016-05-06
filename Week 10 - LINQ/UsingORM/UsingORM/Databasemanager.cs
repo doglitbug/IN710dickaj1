1686008d33d7cab9a06b4c36c5d09e4da38d4d03
doglitbug <dickaj1@student.op.ac.nz>
2016-05-06 21:39:27 +1200

    Added project, db manager and stub for first method

---
+++ Week 10 - LINQ/UsingORM/UsingORM/Databasemanager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingORM
+{
+    public class DatabaseManager
+    {
+        /// <summary>
+        /// Holds connection to MS SQL database
+        /// </summary>
+        private DataClassesDataContext db;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public DatabaseManager()
+        {
+            //Connect to database
+            db = new DataClassesDataContext();
+        }
+
+        public List<String> getAverageIntensty()
+        {
+            //Hold output
+            List<String> output=new List<String>();
+
+            //Provide a title
+            output.Add("1. Compute and display the average intensity for all strikes");
+            output.Add("------------------------------------------------------------");
+
+            return output;
+        }
+    }
+}
---
+++ Week 10 - LINQ/UsingORM/UsingORM/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsingORM
+{
+    public partial class Form1 : Form
+    {
+        DatabaseManager dbm;
+        public Form1()
+        {
+            InitializeComponent();
+            dbm = new DatabaseManager();
+        }
+
+        private void btnAveIntensity_Click(object sender, EventArgs e)
+        {
+            listBox1.DataSource=null;
+            listBox1.DataSource = dbm.getAverageIntensty();
+        }
+    }
+}
