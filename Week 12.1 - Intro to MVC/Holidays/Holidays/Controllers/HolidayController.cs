bf436dc949491883940d279d839ec2a64d7e33ba
doglitbug <dickaj1@student.op.ac.nz>
2016-05-17 21:44:08 +1200

    Added holidays in Controller and pick on at random

---
+++ Week 12.1 - Intro to MVC/Holidays/Holidays/Controllers/HolidayController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Holidays.Models;
+
+namespace Holidays.Controllers
+
+{
+    public class HolidayController : Controller
+    {
+        //
+        // GET: /Index/
+        public ActionResult Index()
+        {
+            //Hold known holidays
+            List<Holiday> holidays = new List<Holiday>();
+
+            //Add holidays
+            holidays.Add(new Holiday("Queen's Birthday",6,6,"queensBirthday.jpg"));//Mondayization here...
+            holidays.Add(new Holiday("Halloween", 31, 10, "halloween.jpg"));
+            holidays.Add(new Holiday("Boxing Day", 26, 12, "boxingDay.jpg"));
+
+            //Choose a holiday at random
+            Random rng = new Random();
+            int holidayIndex = rng.Next(holidays.Count());
+
+            return View(holidays[holidayIndex]);
+        }
+	}
+}
--- Week 12.1 - Intro to MVC/Holidays/Holidays/Models/Holiday.cs
+++ Week 12.1 - Intro to MVC/Holidays/Holidays/Models/Holiday.cs
@@ -31,5 +31,20 @@ namespace Holidays.Models
             //TODO Stubbed out
             return 5;
         }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name">Name of the holiday</param>
+        /// <param name="day">Day of the month</param>
+        /// <param name="month">Month of the year</param>
+        /// <param name="imageName">Filename of the image</param>
+        public Holiday(String name, int day, int month, String imageName)
+        {
+            this.name = name;
+            //TODO Check for invalid date here
+            this.date = new DateTime(DateTime.Now.Year, month, day);
+            this.imageName = imageName;
+        }
     }
 }
