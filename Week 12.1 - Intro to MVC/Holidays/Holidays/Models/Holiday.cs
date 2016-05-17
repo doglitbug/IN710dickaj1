08e66c2ee335a434c7a4b3982e52e71114e35715
doglitbug <dickaj1@student.op.ac.nz>
2016-05-17 21:12:09 +1200

    Added project and stubbed Holiday model

---
+++ Week 12.1 - Intro to MVC/Holidays/Holidays/Models/Holiday.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Holidays.Models
+{
+    public class Holiday
+    {
+        /// <summary>
+        /// Name of the holiday
+        /// </summary>
+        public String name { get; set; }
+
+        /// <summary>
+        /// Date of the holiday
+        /// </summary>
+        public DateTime date { get; set; }
+
+        /// <summary>
+        /// Find out how many days until this holiday occurs
+        /// </summary>
+        /// <returns>Number of days until holiday occurs</returns>
+        public int dayUntil()
+        {
+            //TODO Stubbed out
+            return 5;
+        }
+    }
+}
