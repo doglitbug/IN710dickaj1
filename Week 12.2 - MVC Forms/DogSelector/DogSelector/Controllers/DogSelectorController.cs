be5cba8ad61c0c516e849d374ac2339f9034157e
doglitbug <dickaj1@student.op.ac.nz>
2016-05-21 14:46:45 +1200

    Added most of the code for suggestion page

--- Week 12.2 - MVC Forms/DogSelector/DogSelector/Controllers/DogSelectorController.cs
+++ Week 12.2 - MVC Forms/DogSelector/DogSelector/Controllers/DogSelectorController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DogSelector.Models;
 
 namespace DogSelector.Controllers
 {
@@ -10,9 +11,32 @@ namespace DogSelector.Controllers
     {
         //
         // GET: /DogSelector/
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Dog searchDog)
+        {
+            //TODO Choose a dog in here from what has been provided!
+            Dog suggestedDog = new Dog()
+            {
+                BreedName = "BichonFrise",
+                DisplayName = "Bichon Frise",
+                ActivityLevel = Dog.EScale.High,
+                CoatLength = Dog.ELength.Medium,
+                Drools = false,
+                GoodWithChildren = true,
+                GroomingLevel = Dog.EScale.High,
+                IntelligenceLevel = Dog.EScale.High,
+                SheddingLevel = Dog.EScale.Low,
+                Size = Dog.ESize.Small,
+                ImageName = "Bichonfrise.jpg"
+            };
+
+            return View("Suggestion", suggestedDog);
         }
 	}
 }
